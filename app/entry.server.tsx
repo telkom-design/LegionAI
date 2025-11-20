5de35e79dd703779426ba7dc8090e90e0ccf8a6b
Amalina Kurniasari <amalina.kurniasari@telkom.co.id>
2025-11-20 14:27:04 +0700

    fix error renderToReadableStream not found

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,13 +1,10 @@
 import type { AppLoadContext } from '@remix-run/cloudflare';
 import { RemixServer } from '@remix-run/react';
 import { isbot } from 'isbot';
-import server from 'react-dom/server';
-import { renderToReadableStream } from 'react-dom/server';
+import { renderToReadableStream } from 'react-dom/server.browser';
 import { renderHeadToString } from 'remix-island';
 import { Head } from './root';
 import { themeStore } from '~/lib/stores/theme';
-
-const { renderToReadableStream } = server;
 
 export default async function handleRequest(
   request: Request,
